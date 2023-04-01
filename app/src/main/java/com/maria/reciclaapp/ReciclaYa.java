4c2365365be6d08d578a2cfdbb85d021f12af50f
Jhon capote <capotejhon173@gmail.com>
2023-04-01 12:38:18 -0500

    primer commit

---
+++ app/src/main/java/com/maria/reciclaapp/ReciclaYa.java
@@ -0,0 +1,14 @@
+package com.maria.reciclaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReciclaYa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recicla_ya);
+    }
+}
---
+++ app/src/main/java/com/maria/reciclaapp/tiposApp.java
@@ -0,0 +1,14 @@
+package com.maria.reciclaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tiposApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipos_app);
+    }
+}
